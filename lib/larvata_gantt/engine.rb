8e4ca7f66ed58efc43df8b6f426fe89c1756ccb9
Frank Lam <ryzingsun11@yahoo.com>
2019-04-25 10:09:08 +0800

    Add factory_bot_rails gem
    * Fix factory lookup error:
    https://github.com/thoughtbot/factory_bot_rails/pull/149/files

--- lib/larvata_gantt/engine.rb
+++ lib/larvata_gantt/engine.rb
@@ -1,5 +1,15 @@
 module LarvataGantt
   class Engine < ::Rails::Engine
     isolate_namespace LarvataGantt
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+    end
+
+    initializer "model_core.factories", :after => "factory_bot.set_factory_paths" do
+      FactoryBot.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryBot)
+    end
   end
 end
