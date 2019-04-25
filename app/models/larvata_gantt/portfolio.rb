3cbf28bf207e1afab8b98de803558ff4f7a03c53
Frank Lam <ryzingsun11@yahoo.com>
2019-04-25 14:32:32 +0800

    Specify foreign key option for entity association

--- app/models/larvata_gantt/portfolio.rb
+++ app/models/larvata_gantt/portfolio.rb
@@ -1,5 +1,5 @@
 module LarvataGantt
   class Portfolio < ApplicationRecord
-    belongs_to :entity
+    belongs_to :entity, class_name: LarvataGantt.entity_class.to_s, foreign_key: 'larvata_gantt_entity_id'
   end
 end
