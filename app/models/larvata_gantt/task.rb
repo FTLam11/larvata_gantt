66a10f5c8282429a3ffb062ef3fbb35276c58538
Frank Lam <ryzingsun11@yahoo.com>
2019-04-25 18:00:07 +0800

    Add specs for Task model associations

--- app/models/larvata_gantt/task.rb
+++ app/models/larvata_gantt/task.rb
@@ -1,6 +1,6 @@
 module LarvataGantt
   class Task < ApplicationRecord
-    belongs_to :portfolio
-    belongs_to :owner, class_name: LarvataGantt.owner_class.to_s
+    belongs_to :portfolio, foreign_key: 'larvata_gantt_portfolio_id'
+    belongs_to :owner, class_name: LarvataGantt.owner_class.to_s, foreign_key: 'user_id', optional: true
   end
 end
--- spec/models/larvata_gantt/task_spec.rb
+++ spec/models/larvata_gantt/task_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 module LarvataGantt
   RSpec.describe Task, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'belongs to a portfolio' do
+      task = build(:larvata_gantt_task)
+
+      expect(task.portfolio).to_not be nil
+    end
+
+    it 'may be owner-less' do
+      task = build(:larvata_gantt_task)
+
+      expect(task.owner).to be nil
+    end
+
+    it 'may be belong to an owner' do
+      owner = build(:owner)
+
+      task = build(:larvata_gantt_task, owner: owner)
+
+      expect(task.owner).to be(owner)
+    end
   end
 end
