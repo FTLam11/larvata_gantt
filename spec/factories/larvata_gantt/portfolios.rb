4f2e09443c8f0421deabb9e1d671920735fb37ae
Frank Lam <ryzingsun11@yahoo.com>
2019-04-29 15:46:08 +0800

    Update task and portfolio factories

--- spec/factories/larvata_gantt/portfolios.rb
+++ spec/factories/larvata_gantt/portfolios.rb
@@ -12,5 +12,15 @@ FactoryBot.define do
         create_list(:larvata_gantt_task, evaluator.tasks_count, portfolio: portfolio)
       end
     end
+
+    factory :larvata_gantt_portfolio_with_nil_start_date_tasks, class: 'LarvataGantt::Portfolio' do
+      transient do
+        tasks_count { 3 }
+      end
+
+      after(:create) do |portfolio, evaluator|
+        create_list(:larvata_gantt_task, evaluator.tasks_count, portfolio: portfolio, start_date: nil)
+      end
+    end
   end
 end
--- spec/factories/larvata_gantt/tasks.rb
+++ spec/factories/larvata_gantt/tasks.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  sequence :started_at do |n|
+    n.days.ago
+  end
+
   factory :larvata_gantt_task, class: 'LarvataGantt::Task' do
     portfolio
     owner { nil }
@@ -7,8 +11,21 @@ FactoryBot.define do
     typing { 1 }
     priority { 1 }
     progress { 0 }
-    end_date { "2019-04-25 17:27:33" }
-    start_date { "2019-04-25 17:27:33" }
-    text { "First task" }
+    end_date { 1.week.from_now }
+    start_date { generate(:started_at) }
+    text { 'First task' }
+  end
+
+  factory :larvata_gantt_project, class: 'LarvataGantt::Project' do
+    portfolio
+    owner { nil }
+    sort_order { 1 }
+    parent { nil }
+    typing { 0 }
+    priority { 1 }
+    progress { 0 }
+    end_date { nil }
+    start_date { nil }
+    text { 'Project Operation Diam La' }
   end
 end
