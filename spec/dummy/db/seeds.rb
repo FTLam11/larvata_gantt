e7edc84dad69c108677c2aa84ba0f2ccde231fd5
Frank Lam <ryzingsun11@yahoo.com>
2019-04-30 17:10:46 +0800

    Add link to seeds

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -4,7 +4,8 @@ portfolio = LarvataGantt::Portfolio.create!(entity: entity, name: 'Take over wor
 owner = User.create!(name: 'Fronk')
 project = LarvataGantt::Project.create!(owner: owner, typing: LarvataGantt::BasicTask.typings[:project],
                                         text: 'Dominate world', portfolio: portfolio)
-LarvataGantt::Task.create!(owner: owner, typing: LarvataGantt::BasicTask.typings[:task],
-                           text: 'Secure the bag', parent: project.id.to_s,
-                           start_date: Time.zone.now, end_date: 1.week.from_now)
+task = LarvataGantt::Task.create!(owner: owner, typing: LarvataGantt::BasicTask.typings[:task],
+                                  text: 'Secure the bag', parent: project.id.to_s,
+                                  start_date: Time.zone.now, end_date: 1.week.from_now, portfolio: portfolio)
+LarvataGantt::Link.create!(source: project, target: task, lag: 0, typing: LarvataGantt::Link.typings[:finish_to_finish])
 puts "Seed finished"
