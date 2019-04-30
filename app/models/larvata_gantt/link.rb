97b1440f9b733d86a97229bb7348f1574e487ca4
Frank Lam <ryzingsun11@yahoo.com>
2019-04-30 16:53:29 +0800

    Test Link#lag

--- app/models/larvata_gantt/link.rb
+++ app/models/larvata_gantt/link.rb
@@ -4,6 +4,7 @@ module LarvataGantt
     belongs_to :target, class_name: "LarvataGantt::BasicTask"
 
     validates_presence_of :source_id, :target_id
+    validates :lag, numericality: { greater_than_or_equal_to: 0 }
     CONSTRAINTS = %i(asap alap snet snlt fnet fnlt mso mfo).freeze
     TYPINGS = %i(finish_to_start start_to_start finish_to_finish start_to_finish).freeze
     enum typing: TYPINGS
--- spec/models/larvata_gantt/link_spec.rb
+++ spec/models/larvata_gantt/link_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 module LarvataGantt
-  RSpec.describe Link, type: :model do
+  RSpec.describe(Link, type: :model) do
     it 'has source and target tasks' do
       project = create(:project)
       child_task = create(:task, parent: project)
 
       link = create(:link, source: project, target: child_task)
 
-      expect(link.source).to eq(project)
-      expect(link.target).to eq(child_task)
+      expect(link.source).to(eq(project))
+      expect(link.target).to(eq(child_task))
+    end
+
+    it 'may have lag time' do
+      project = create(:project)
+      child_task = create(:task, parent: project)
+
+      link = create(:link, source: project, target: child_task, lag: 2)
+
+      expect(link.lag).to(eq(2))
+      expect { create(:link, source: project, target: child_task, lag: -1) }.to(raise_error(ActiveRecord::RecordInvalid))
     end
   end
 end
