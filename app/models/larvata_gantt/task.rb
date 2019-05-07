d878f509cf8b441510a4bf04061eebf6371519c5
Frank Lam <ryzingsun11@yahoo.com>
2019-05-07 16:08:19 +0800

    Include Durationable in Task

--- app/models/larvata_gantt/task.rb
+++ app/models/larvata_gantt/task.rb
@@ -1,19 +1,12 @@
 module LarvataGantt
   class Task < BasicTask
-    validates_presence_of :priority, :progress, :start_date, :end_date
+    include LarvataGantt::Durationable
+
+    validates_presence_of :priority, :progress
     validates :progress, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-    validate :end_date_is_later_than_start_date
 
     def post_initialize(*)
       nil
-    end
-
-    private
-
-    def end_date_is_later_than_start_date
-      if end_date && start_date && end_date < start_date
-        errors.add("End date", "can't be later than start date")
-      end
     end
   end
 end
