1efba1e5e8bf215c1b492cb4239ddea7a400a1d6
Frank Lam <ryzingsun11@yahoo.com>
2019-05-08 12:16:14 +0800

    Downgrade Rails version

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,7 +9,7 @@ require "larvata_gantt"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,4 +17,3 @@ module Dummy
     # the framework and any gems in your application.
   end
 end
-
